920485ce182a8789a6ea6da73bbf88383cd4aea2
wei chen <weichen9083@gmail.com>
2021-05-30 00:43:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,23 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
+	
+	
+		get '/hello' do
+		erb :hello
+	end
+	
+	get '/hello' do
+		erb :hello
+	end
+	get '/goodbye' do
+		erb :goodbye
+	end
+	
+		get '/date' do
+		erb :date
+	end
+	
 
 
 end
